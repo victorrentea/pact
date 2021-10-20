0ef94f9ebbef9fe83a6bea24d88b800e560c2bb4
Victor Rentea <victorrentea@gmail.com>
2021-10-21 02:38:03 +0300

    END DONE

--- provider/src/test/java/victor/training/pact/provider/PactVerificationTest.java
+++ provider/src/test/java/victor/training/pact/provider/PactVerificationTest.java
@@ -6,8 +6,7 @@ import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvide
 import au.com.dius.pact.provider.junitsupport.IgnoreMissingStateChange;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
-import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,10 +18,9 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Provider("ProductService")
-//@PactFolder("pacts")
+@PactFolder("pacts")
 @IgnoreMissingStateChange
-@PactBroker(host="localhost", port = "9292",
-    authentication = @PactBrokerAuth(username = "pact_workshop", password = "pact_workshop"))
+//@PactBroker(host="localhost", port = "9292",  authentication = @PactBrokerAuth(username = "pact_workshop", password = "pact_workshop"))
 public class PactVerificationTest {
 
    @LocalServerPort
